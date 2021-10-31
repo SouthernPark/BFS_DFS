2898d12cea5f959322338684959fcab5b352d35e
SouthernPark <2848919007@qq.com>
2021-10-31 14:37:22 -0400

    finished p2stack

--- p2stack.py
+++ p2stack.py
@@ -13,6 +13,8 @@ Date:
 """
 Stack Class
 """
+
+
 class Stack:
 
     """
@@ -28,15 +30,20 @@ class Stack:
     Note: the stack is initally filled with None values.
     Note: since nothing is on the stack, top is -1.
     """
+
     def __init__(self, size=3):
-        self.stack = [None for x in range(0,size)]
+        # init the array for the stack
+        self.stack = [None for x in range(0, size)]
+        # index of the top element
         self.top = -1
+        # numElems record how many elements have been put into stack
         self.numElems = 0
         return
 
     """
     __repr__ function to print the stack.
     """
+
     def __repr__(self):
         s = '[ ' + ', '.join(map(str, self.stack)) + ' ]\n'
         s += ('Top: %d' % self.top) + '\n'
@@ -46,52 +53,62 @@ class Stack:
     """
     isFull function to check if the stack is full.
     """
+
     def isFull(self):
         ##### IMPLEMENT! #####
-        if len(self.stack) == self.numElems:
+        # check if numElements is equal to len of the array
+        if(self.numElems == len(self.stack)):
             return True
         return False
 
     """
     isEmpty function to check if the stack is empty.
     """
+
     def isEmpty(self):
         ##### IMPLEMENT! #####
-        if self.numElems == 0:
+        if(self.numElems == 0):
             return True
         return False
 
     """
     resize function to resize the stack by doubling its size.
     """
+
     def resize(self):
         ##### IMPLEMENT! #####
-        temp_arr = [None for x in range(0, 2 * self.numElems)]
-        for i in range(0, self.numElems):
-            temp_arr[i] = self.stack[i]
-        self.stack = temp_arr
+        temp = [None for i in range(len(self.stack)*2)]
+        # copy the elements in stack to temp
+        for i in range(self.numElems):
+            temp[i] = self.stack[i]
+        self.stack = temp
         return
 
     """
     push function to push a value onto the stack.
     """
+
     def push(self, val):
         ##### IMPLEMENT! #####
-        if self.isFull():
+        # check whether the stack is full
+        if(self.isFull()):
             self.resize()
         self.top += 1
-        self.numElems += 1
         self.stack[self.top] = val
+        self.numElems += 1
         return
 
     """
     pop function to pop the value off the top of the stack.
     """
+
     def pop(self):
         ##### IMPLEMENT! #####
-        if self.top < 0:
+        if(self.numElems == 0 or self.top < 0):
             return None
 
+        res = self.stack[self.top]
         self.top -= 1
         self.numElems -= 1
-        return self.stack[self.top + 1]
+
+        return res
